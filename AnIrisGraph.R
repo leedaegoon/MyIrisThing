fd35230f95981a9de043af22163be097abf39d1e
leedaegoon <leedaegoon@gmail.com>
2020-05-16 10:24:08 -0700

    I changed the title.

--- AnIrisGraph.R
+++ AnIrisGraph.R
@@ -4,6 +4,6 @@ sd(iris$Sepal.Length)
 my.plot <- ggplot(data=iris, aes(x=Sepal.Length,
                       y=Sepal.Width, color = Species)) +
   geom_point(size=5) +
-  ggtitle("My iris graph")
+  ggtitle("My iris graph 2")
 
 ggsave(filename="C:/User/Daegoon Lee/Desktop/MyNewProject/myIrisgraph.png")
