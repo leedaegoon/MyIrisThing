952d37c9d4b64b31bfe58d76e1b7a697634a556b
leedaegoon <leedaegoon@gmail.com>
2020-05-16 10:14:39 -0700

    This is my second commit. I saved the graph.

--- AnIrisGraph.R
+++ AnIrisGraph.R
@@ -1,6 +1,9 @@
 mean(iris$Sepal.Length)
 sd(iris$Sepal.Length)
 
-ggplot(data=iris, aes(x=Sepal.Length,
-                      y=Sepal.Width), color = Species))+
-  geom_point()
+my.plot <- ggplot(data=iris, aes(x=Sepal.Length,
+                      y=Sepal.Width, color = Species)) +
+  geom_point(size=5) +
+  ggtitle("My iris graph")
+
+ggsave(filename="C:/User/Daegoon Lee/Desktop/MyNewProject/myIrisgraph.png")
